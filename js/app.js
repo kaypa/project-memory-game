49efc8044dab998bfbac3a7bc33cb33b8cb0d064
Kariz Alberto <kariz.alberto@yahoo.com>
2018-06-07 18:21:38 -0700

    Makes sure that only closed cards can be clicked

--- js/app.js
+++ js/app.js
@@ -69,3 +69,11 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+cardDeck.addEventListener('click', function(evt) {
+  if (evt.target.nodeName === 'LI') {
+    if (!evt.target.classList.contains('show') && !evt.target.classList.contains('open') && !evt.target.classList.contains('match')) {
+
+    }
+  }
+});
